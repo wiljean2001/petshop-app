8cb24ea850351095fdcd9cdc4f847f7a295b67b1
wiljean2001 <86029273+wiljean2001@users.noreply.github.com>
2024-01-12 14:30:21 -0500

    admin/services/page.tsx
    commented ServiceDetails extract

--- src/app/[lang]/(home)/admin/services/page.tsx
+++ src/app/[lang]/(home)/admin/services/page.tsx
@@ -42,7 +42,7 @@ export default async function ServicePage({ searchParams }: Props) {
       duration: true,
       // image: true,
       state: true,
-      ServiceDetails: true,
+      // ServiceDetails: true,
       ServiceAppointments: true,
       requiresClinicalData: true,
       createdAt: true,
